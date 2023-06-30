c078d8f15d4f1ad22e341d0cc304a135f8fdd30f
daniNKV <nikiforovdaniel@gmail.com>
2023-06-30 12:47:45 -0300

    Add second exercise

--- src/index-mio.js
+++ src/index-mio.js
@@ -1,22 +1,34 @@
-// import { Tarjeta } from "./tarea/Tarea1.js";
-
+// import { Tarjeta, BlogPost } from "./tarea/Tarea1.js";
 // ReactDOM.render(<Tarjeta
 //     nombre="Daniel Nikiforov"
 //     profesion="Programador"
 //     imagen="https://avatars2.githubusercontent.com/u/14203988?s=460&v=4"
 // />, document.getElementById('react-app'));
 
-import { BlogPost } from "./tarea/Tarea1.js";
+// ReactDOM.render(<BlogPost 
+//     titulo="Ardillas"
+//     parrafos={`Hoy vi una ardilla.
+//         La ardilla era negra, era más grande que otras ardillas, tenía muchos dientes grandes y encima andaba siempre en cuatro patas, moviendo la cola.
+//         Creo que puede haber sido un perro, dado que en Argentina no hay ardillas.`
+//     }
+//     autor={{
+//         nombre: "Daniel",
+//         titulo: "Primer Post",
+//         imagen: "https://avatars2.githubusercontent.com/u/14203988?s=460&v=4",
+//     }}
+// />, document.getElementById("react-app"));
 
-ReactDOM.render(<BlogPost 
-    titulo="Ardillas"
-    parrafos={`Hoy vi una ardilla.
-        La ardilla era negra, era más grande que otras ardillas, tenía muchos dientes grandes y encima andaba siempre en cuatro patas, moviendo la cola.
-        Creo que puede haber sido un perro, dado que en Argentina no hay ardillas.`
-    }
-    autor={{
-        nombre: "Daniel",
-        titulo: "Primer Post",
-        imagen: "https://avatars2.githubusercontent.com/u/14203988?s=460&v=4",
-    }}
-/>, document.getElementById("react-app"));
+
+// import { MatchNombre, PasswordInput, ValidationInput } from "./tarea/Tarea2.js";
+
+// ReactDOM.render(<MatchNombre nombre="Daniel"/>, document.getElementById('react-app'));
+
+// ReactDOM.render(<PasswordInput minLength="4"/>, document.getElementById('react-app'));
+
+// const validateLength = (value) => value.length >= 8;
+
+// ReactDOM.render(<ValidationInput 
+//     validation={validateLength}
+//     isPassword={false}
+//     />, document.getElementById('react-app'));
+
